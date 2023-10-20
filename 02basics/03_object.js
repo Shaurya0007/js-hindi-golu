ca734bc37d0760f6138ab0be2c6af115463ff906
shaurya singh <141993563+Shaurya0007@users.noreply.github.com>
2023-10-20 11:36:59 +0000

    javascript arrays 02

---
+++ 02basics/03_object.js
@@ -0,0 +1,11 @@
+const jsUser = {
+    name:"hithsh",
+    email: "kumarshaurya168@gmail.com",
+    phoneNo: 6206807909,
+    isLoggedIn : false,
+    lasatLoggedIN : ['monday','saturday']
+}
+
+console.log(jsUser);
+console.log(jsUser.email);
+console.log(jsUser['email'])
