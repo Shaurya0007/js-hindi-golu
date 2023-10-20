123348b3903ad2c40cc313ae512233a2b3e9b8d4
shaurya singh <141993563+Shaurya0007@users.noreply.github.com>
2023-10-20 11:35:04 +0000

    javascript vreiables

---
+++ 01basics/01veribales.js
@@ -0,0 +1,10 @@
+const userAccount = 12445
+let userEmail = "golu@1234.com"
+var userPassward = "12345"
+
+
+console.table([userAccount,userEmail,userPassward ]);
+
+
+
+[[][][[[[[[[[[[[[[][]]]]]]]]]]]]]]
