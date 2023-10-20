70f7a464d1eec84a8e6c1ddad4cb4c3e92c09179
shaurya singh <141993563+Shaurya0007@users.noreply.github.com>
2023-10-20 11:34:34 +0000

    test strings

---
+++ 01basics/01test_strings.js
@@ -0,0 +1,16 @@
+let str1 = "hello"
+let str2 = " golu"
+let str3 = " how"
+let str4 = " are you"
+
+let str5 = str1+str2+str3+str4
+console.log(str5); 
+
+
+let a = "hi"
+let b = " golu"
+let c = " what"
+let d = "happend"
+let e = " any problem ?"
+let f = a+b+c+d+e
+console.log(f);
